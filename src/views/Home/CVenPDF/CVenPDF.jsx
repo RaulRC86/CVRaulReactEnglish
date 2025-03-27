0ba746dab9339f934c49cb7b98d23c06b7a3778b
Raúl <raul.rodriguez.c.2012@gmail.com>
2025-03-27 13:32:27 -0300

    addapting CV in  PDF

--- src/views/Home/CVenPDF/CVenPDF.jsx
+++ src/views/Home/CVenPDF/CVenPDF.jsx
@@ -3,7 +3,7 @@ import './cVenPDF.css'
 export const CVenPDF = () => {
   return (
     <div className="cvDescargable">
-    <a href="/CVRaúlRodríguez2025.pdf" target="_blank"
+    <a href="/CVRaúlRodríguez2025Eng.pdf" target="_blank"
       >⬇ Download my CV in PDF format! ⬇</a>
   </div>
   )
